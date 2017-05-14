312e80179565ec97f804bcd2eb739087ec109746
Trachifab <fabiendorey@gmail.com>
2017-05-14 17:53:38 +0200

    Init with seed

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>yo</div>
+        );
+    }
+}
+
+export default PostsIndex;
