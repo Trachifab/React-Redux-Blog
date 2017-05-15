b8730d12700f96d0a5af839c1b264ee2d14e7d15
Trachifab <fabiendorey@gmail.com>
2017-05-15 18:52:31 +0200

    Create a post (api got a cors issue)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,30 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=DUMMYKEYFORTESTING'
 
+const config = {
+  headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+};
+
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`, config);
 
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values, config)
+        .then(() => callback());
+
+    return {
+        type: CREATE_POST,
         payload: request
     };
 }
